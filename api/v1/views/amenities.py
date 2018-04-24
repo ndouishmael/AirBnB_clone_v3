aa70b2b9b4c860ef8ca043635493cb5eed653bf6
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-24 16:01:33 -0700

    creating routes for amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 ''' blueprint for state '''
 from api.v1.views import app_views
 from flask import jsonify, abort, request
@@ -37,63 +36,40 @@ def delete_amenity(a_id):
     amenities.delete()
     return jsonify({})
 
-# @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
-# def get_city(city_id):
-#     ''' Retrieve city my city_id'''
 
-#     my_city = storage.get('City', city_id)
-#     if my_city is None:
-#         abort(404)
-#     return jsonify(my_city.to_dict())
+@app_views.route('/amenities', methods=["POST"], strict_slashes=False)
+def create_amenity():
+    ''' Creates an Amenity '''
+    try:
+        content = request.get_json()
+    except:
+        return (jsonify({"error": "Not a JSON"}), 400)
+    name = content.get("name")
+    if name is None:
+        return (jsonify({"error": "Missing name"}), 400)
 
-
-# @app_views.route('/cities/<c_id>', methods=["DELETE"], strict_slashes=False)
-# def delete_cities(c_id):
-
-#     my_city = storage.get("City", c_id)
-#     if my_city is None:
-#         abort(404)
-#     storage.delete(my_city)
-#     return (jsonify({}))
+    new_amenity = Amenity()
+    new_amenity.name = name
+    new_amenity.save()
+    return (jsonify(new_amenity.to_dict()), 201)
 
 
-# @app_views.route('/states/<id>/cities', methods=["POST"], strict_slashes=False)
-# def post_cities(id):
-#     ''' Post a new city '''
-#     try:
-#         content = request.get_json()
-#     except:
-#         return (jsonify({"error": "Not a JSON"}), 400)
-#     name = content.get("name")
-#     if name is None:
-#         return (jsonify({"error": "Missing name"}), 400)
-#     my_state = storage.get('State', id)
-#     if my_state is None:
-#         abort(404)
-#     new_city = City()
-#     new_city.state_id = id
-#     new_city.name = name
-#     new_city.save()
+@app_views.route('/amenities/<a_id>', methods=["PUT"], strict_slashes=False)
+def update_amenity(a_id):
+    ''' updates an amenity '''
 
-#     return (jsonify(new_city.to_dict()), 201)
+    try:
+        content = request.get_json()
+    except:
+        return (jsonify({"error": "Not a JSON"}), 400)
 
+    my_amenity = storage.get("Amenity", a_id)
+    if my_amenity is None:
+        abort(404)
 
-# @app_views.route('/cities/<city_id>', methods=["PUT"], strict_slashes=False)
-# def update_cities(city_id):
-#     ''' update city object attributes with PUT method'''
+    for key, value in content.items():
+        if key != "id" or key != "created_at" or key != "updated_at":
+            setattr(my_amenity, key, value)
 
-#     try:
-#         content = request.get_json()
-#     except:
-#         return (jsonify({"error": "Not a JSON"}), 400)
-
-#     my_city = storage.get("City", city_id)
-#     if my_city is None:
-#         abort(404)
-
-#     for key, value in content.items():
-#         if key != "id" or key != "created_at" or key != "updated_at":
-#             setattr(my_city, key, value)
-
-#     my_city.save()
-#     return jsonify(my_city.to_dict())
+    my_amenity.save()
+    return jsonify(my_amenity.to_dict())
