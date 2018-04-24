151533ed163828afd055aa54bf543f99de9c4b3c
rayraib <binitarai11@gmail.com>
2018-04-24 17:51:27 +0000

    handle 404 error with json

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 '''
    contain teardown method
 '''
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,12 +13,19 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.errorhandler(404)
+def handle_404(error):
+    ''' custom JSON 404 error'''
+    return (jsonify({"error": "Not found"}), 404)
+
+
 @app.teardown_appcontext
 def teardown(exception):
     '''
         Teardown method for storage session
     '''
     storage.close()
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
