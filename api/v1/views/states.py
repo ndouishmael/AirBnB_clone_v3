54bab9514d4084ed7468aa2c550feff1119c573d
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-25 12:08:27 -0700

    adding  stict slashes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,13 +7,13 @@ from models import State
 
 
 @app_views.route('/states', methods=["GET"], strict_slashes=False)
-@app_views.route('/states/<state_id>', methods=["GET"])
+@app_views.route('/states/<state_id>', methods=["GET"], strict_slashes=False)
 def state(state_id=None):
     ''' retrieves a list of all states'''
     if state_id is None:
         states = storage.all("State")
         my_states = [value.to_dict() for key, value in states.items()]
-        return (jsonify(my_states), 200)
+        return jsonify(my_states)
 
     my_states = storage.get("State", state_id)
     if my_states is not None:
@@ -21,7 +21,7 @@ def state(state_id=None):
     abort(404)
 
 
-@app_views.route('/states/<s_id>', methods=["DELETE"])
+@app_views.route('/states/<s_id>', methods=["DELETE"], strict_slashes=False)
 def delete_states(s_id):
     '''Deletes an specific state based on its id'''
 
@@ -29,7 +29,7 @@ def delete_states(s_id):
     if my_state is None:
         abort(404)
     storage.delete(my_state)
-    return (jsonify({}), 200)
+    return (jsonify({}))
 
 
 @app_views.route('/states', methods=["POST"], strict_slashes=False)
@@ -38,36 +38,33 @@ def post_states():
     try:
         content = request.get_json()
     except:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        return (jsonify({"error": "Not a JSON"}), 400)
     name = content.get("name")
     if name is None:
-        return make_response(jsonify({"error": "Missing name"}), 400)
+        return (jsonify({"error": "Missing name"}), 400)
 
     new_state = State()
-    for key, value in content.items():
-        setattr(new_state, key, value)
-
+    new_state.name = name
     new_state.save()
 
     return (jsonify(new_state.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=["PUT"])
+@app_views.route('/states/<state_id>', methods=["PUT"], strict_slashes=False)
 def update_states(state_id):
     '''Updates a state'''
 
     try:
         content = request.get_json()
     except:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        return (jsonify({"error": "Not a JSON"}), 400)
 
     my_state = storage.get("State", state_id)
     if my_state is None:
         abort(404)
 
-    not_allowed = ["id", "created_at", "updated_at"]
     for key, value in content.items():
-        if key not in not_allowed:
+        if key != "id" or key != "created_at" or key != "updated_at":
             setattr(my_state, key, value)
 
     my_state.save()
